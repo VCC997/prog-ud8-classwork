767ec315cd415536fc5f7cb5d0333ea07b38d949
Víctor <33480572+VCC997@users.noreply.github.com>
2020-02-26 00:00:21 +0100

    Polimorfismo I
    Sobreescribimos  el método equals
    de la clase Multimedia de forma que retorne que 2 elementos son iguales si su título y autor son los
    mismos.

---
+++ src/actividad5/Formato.java
@@ -0,0 +1,33 @@
+package actividad5;
+
+public class Formato {
+
+    public enum Format{WAV,MIDI,AVI,MP3,MPG,MOV,cdAudio,DVD};
+    private Format formato;
+
+    public void chooseFormat(Format formato, int formatos){
+
+        switch(formatos){
+            case 1: this.formato = Format.WAV;
+                break;
+            case 2: this.formato = Format.MIDI;
+                break;
+            case 3: this.formato = Format.AVI;
+                break;
+            case 4: this.formato = Format.MP3;
+                break;
+            case 5: this.formato = Format.MPG;
+                break;
+            case 6: this.formato = Format.MOV;
+                break;
+            case 7: this.formato = Format.cdAudio;
+                break;
+            default: this.formato = Format.DVD;
+                break;
+        }
+    }
+
+
+
+
+}
---
+++ src/actividad5/Pelicula.java
@@ -0,0 +1,38 @@
+package actividad5;
+
+public class Pelicula extends Multimedia {
+
+    private String actorPrincipal;
+    private String actrizPrincipal;
+
+
+    public Pelicula(String actorPrincipal, String actrizPrincipal, String tittle, String author, Formato format, int duration){
+        super(tittle, author, format,duration);
+        this.actorPrincipal=actorPrincipal;
+        this.actrizPrincipal=actrizPrincipal;
+    }
+
+    public Pelicula(String tittle, String author, Formato format, int duration, String actrizPrincipal){
+        super(tittle, author, format,duration);
+        this.actrizPrincipal=actrizPrincipal;
+    }
+
+    public Pelicula(String actorPrincipal, String tittle, String author, Formato format, int duration){
+        super(tittle, author, format,duration);
+        this.actorPrincipal=actorPrincipal;
+    }
+
+    public String getActorPrincipal(){
+        return actorPrincipal;
+    }
+
+    public String getActrizPrincipal() {
+        return actrizPrincipal;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString()+"actriz"+actrizPrincipal+"actor"+actorPrincipal;
+    }
+}
